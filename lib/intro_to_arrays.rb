54ffe3b606f5077ae09d154b8d731e241904efe7
Miles Marillo <mjm03@hampshire.edu>
2019-03-28 18:57:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,20 @@
+def instantiate_new_array
+  ina = Array.new
+end
+def array_with_two_elements
+  awte = ["one", "two"]
+end
+def first_element(argument)
+  myArray = ["Welcome to New York", "how", "are", "you"]
+  myArray[0]
+end
+def third_element(argument)
+  myArray = ["hello", "how", "Style", "you"]
+  myArray[2]
+end
+def last_element(argument)
+  myArray = ["hello", "how", "are", "Out of The Woods"]
+  myArray[-1]
+end
+def first_element_with_array_methods
+  
